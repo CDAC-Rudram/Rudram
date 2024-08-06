b099a09bad89bb1807b5a0c767b6599cd54f48b6
Rushikesh Bangar <rushibangar6666@gmail.com>
2024-08-06 14:01:15 +0530

    Rudran_BackEnd
    BackEnd Code

---
+++ Project-Rudram/Rudram/src/main/java/com/app/RudramApplication.java
@@ -0,0 +1,29 @@
+package com.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RudramApplication {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		SpringApplication.run(RudramApplication.class, args);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
